6c18ed67bde23363649bcd38a5ee0a32cdbda67b
Lee Min Waan <onenew553@gmail.com>
2025-07-22 22:40:55 +0700

    Initial Runtime Successfully

--- install.sh
+++ install.sh
@@ -6,9 +6,6 @@ sudo apt update
 # Install debootstrap
 sudo apt-get install debootstrap
 
-# Create a debian boostrap lowerdir (directory for the debian package)
-mkdir -p images/debian_rootfs
-
 # Init filesystem in lowerdir
 sudo debootstrap --arch=amd64 stable /var/lib/shipper/crates/debian_rootfs http://deb.debian.org/debian/
 
